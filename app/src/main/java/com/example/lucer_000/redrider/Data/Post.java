f2c76ada49384d800f33fef5f725a08fec8bdeac
Cade Wall <cade.wall@gmail.com>
2018-10-23 19:05:24 -0500

    asdfjkl;

--- app/src/main/java/com/example/lucer_000/redrider/Data/Post.java
+++ app/src/main/java/com/example/lucer_000/redrider/Data/Post.java
@@ -1,6 +1,12 @@
 package com.example.lucer_000.redrider.Data;
-
-
 abstract public class Post {
+	String Date;
 
+	public String getDate() {
+		return Date;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
 }
