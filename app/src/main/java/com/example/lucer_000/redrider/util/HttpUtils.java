f9775511aef21af4cac6af32c03df3a660e8f701
David Lucero <david.lucero@ttu.edu>
2018-11-11 17:20:22 -0600

    commit

--- app/src/main/java/com/example/lucer_000/redrider/util/HttpUtils.java
+++ app/src/main/java/com/example/lucer_000/redrider/util/HttpUtils.java
@@ -20,7 +20,7 @@ public class HttpUtils {
 
     public static HttpUtils myInstance;
     private static Context context;
-    private String URL ="http://10.201.14.137:3000/";
+    private String URL ="http://192.168.1.10:3001/";
 
     public  RequestQueue queue;
 
--- mysqlAPI/server.js
+++ mysqlAPI/server.js
@@ -4,14 +4,7 @@ const cors = require('cors'); //MW so we can make req to our api from different 
 const app = express();
 const mysql = require('mysql');
 const db = require('./dbmanager');
-//Cors Middleware
-// app.use(function (req, res, next) {
-//     //Enabling CORS 
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-//     next();
-// });
+
 
 app.use(cors());
 
@@ -24,25 +17,7 @@ app.get('/', function (req, res) {
     app.send('invlalid endpoint');
 });
 
-function handleResult(result, res) {
-    if (result.querysuccess) {
-        res.json({
-            success: true,
-            body: result.queryresults[0]
-        })
-        success = true;
-        body = result.queryresults[0];
 
-    } else {
-        res.json({
-            success: false,
-            body: result.queryresults
-        })
-        success = false;
-        body = result.queryresults;
-
-    }
-}
 
 app.post('/query', function (req, res, next) {
 
@@ -128,58 +103,53 @@ app.post('/login', function (req, res) {
 
 
     })
-    // var user = {
-    //     email: email,
-    //     password: password
-    // }
+  
 
 
 })
 
 app.post('/signup', function (req, res) {
-    const params = req.body.params;
-
-    const email = params.email;
-    const password = params.password;
-    const major=params.major;
-    const name=params.name;
-    const age=params.age;
-    const sex=params.sex;
+    const email = req.body.email;
+    const password = req.body.password;
+    const major = req.body.major;
+    const name = req.body.name;
+    const age = req.body.age;
+    const sex = req.body.sex;
 
     console.log("email: " + email);
     console.log("password: " + password);
     console.log("major: " + major);
-    console.log("sex: "+ sex);
+    console.log("sex: " + sex);
     console.log("age: " + age);
-    console.log("name: "+name);
+    console.log("name: " + name);
     db.signup(name, email, password, major, age, sex, function (result) {
         //console.log(JSON.stringify(result));
 
         if (result.querysuccess) {
-           success = true;
+            success = true;
             body = result.queryresults;
             res.json({
                 success: true,
                 insertresults: result.queryresults,
-                userid:result.queryresults.insertId
+                userid: result.queryresults.insertId
             })
 
         } else {
             success = false;
             body = result.queryresults;
-            if(result.msg){
+            if (result.msg) {
                 res.json({
-                    success:false,
-                    msg:result.msg
+                    success: false,
+                    msg: result.msg
                 })
-            }else{
+            } else {
                 res.json({
                     success: false,
                     insertresults: result.queryresults
-    
+
                 })
             }
-          
+
 
         }
 
@@ -187,79 +157,79 @@ app.post('/signup', function (req, res) {
     })
 })
 
-app.post('/driverpost',function(req,res){
-    const params = req.body.params;
-    const driverid = params.driverid;
-    const vehicle = params.vehicle;
-    const seats=params.seats;
-    const destination=params.destination;
-    const time=params.time;
-    const date=params.date;
-    db.driverpost(driverid,vehicle,destination,time,date,seats,function (result) {
+app.post('/driverpost', function (req, res) {
+
+    const driverid = req.body.driverid;
+    const vehicle = req.body.vehicle;
+    const seats = req.body.seats;
+    const destination = req.body.destination;
+    const time = req.body.time;
+    const date = req.body.date;
+
+    db.driverpost(driverid, vehicle, destination, time, date, seats, function (result) {
         if (result.querysuccess) {
-           success = true;
+            success = true;
             body = result.queryresults;
             res.json({
                 success: true,
-                postId:body.insertId,
+                postId: body.insertId,
                 insertresults: result.queryresults,
             })
         } else {
             success = false;
             body = result.queryresults;
-            }
-        })
+        }
+    })
 })
 
-app.post('/riderpost',function(req,res){
-    const params = req.body.params;
-   
-    const riderid=params.riderid;
-    const destination=params.destination;
-    const time=params.time;
-    const date=params.date;
-    db.riderpost(riderid,date,destination,time,function (result) {
+app.post('/riderpost', function (req, res) {
+
+    const riderid = req.body.riderid;
+    const destination = req.body.destination;
+    const time = req.body.time;
+    const date = req.body.date;
+    db.riderpost(riderid, date, destination, time, function (result) {
         if (result.querysuccess) {
-           success = true;
+            success = true;
             body = result.queryresults;
             res.json({
                 success: true,
-                postId:body.insertId,
+                postId: body.insertId,
                 insertresults: result.queryresults,
             })
         } else {
             success = false;
             body = result.queryresults;
-            }
-        })
+        }
+    })
 })
 
-app.post('/getposts',function(req,res){
-const params=req.body.params;
-const userid=params.userId
+app.post('/getposts', function (req, res) {
 
-db.getposts(userid,function(result){
-    if (result.querysuccess) {
-        success = true;
-         body = result.queryresults;
-        // console.log(result);
-         res.json({
-             success: true,
-             //postId:body.insertId,
-             riderposts:result.queryresultrider,
-             driverpost: result.queryresultdriver
-         })
-     } else {
-         success = false;
-         body = result.queryresults;
-         }
+    const userid = req.body.userId
+
+    db.getposts(userid, function (result) {
+        if (result.querysuccess) {
+            success = true;
+            body = result.queryresults;
+            // console.log(result);
+            res.json({
+                success: true,
+                //postId:body.insertId,
+                riderposts: result.queryresultrider,
+                driverpost: result.queryresultdriver
+            })
+        } else {
+            success = false;
+            body = result.queryresults;
+        }
+    })
+
 })
 
-})
 
 
-
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('Listening on port 3000.');
 })
 
